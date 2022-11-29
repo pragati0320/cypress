ab0a14695c26dbf38face3f2ead5619e147678bb
Pragati Sapkota <pragatisapkota@Pragatis-MacBook-Pro.local>
2022-11-29 13:04:25 +0545

    selecting searchbox and text on box

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -2,10 +2,13 @@
 
 it('google search' , function(){
 
-    cy.visit('https://google.com')
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
 
-    cy.get('.gLFyf').type('Testing tuorials{Enter}')
+    //On search box, type Cau (Finding Locators)
+    cy.get('.search-keyword').type('Ca{Enter}')
 
+    cy.wait(2000)
+    cy.get('.product:visible').should('have.length',4)
 
 
 
